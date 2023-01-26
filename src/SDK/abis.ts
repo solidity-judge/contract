03aab363922397555e0def4e374a4bf011186d4c
Le Duy Thuc <leduythuc.cs@gmail.com>
2023-01-26 13:57:24 +0700

    Rename gate

--- src/SDK/abis.ts
+++ src/SDK/abis.ts
@@ -1,5 +1,5 @@
 import { abi as IProblemAbi } from '../../build/artifacts/src/interfaces/IProblem.sol/IProblem.json';
-import { abi as IGateAbi } from '../../build/artifacts/src/interfaces/IGate.sol/IGate.json';
+import { abi as IGateAbi } from '../../build/artifacts/src/contracts/Gate.sol/Gate.json';
 import { abi as IUserGateFactoryAbi } from '../../build/artifacts/src/interfaces/IUserGateFactory.sol/IUserGateFactory.json';
 import { abi as IProblemFactoryAbi } from '../../build/artifacts/src/interfaces/IProblemFactory.sol/IProblemFactory.json';
 
