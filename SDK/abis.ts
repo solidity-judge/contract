4aa707b37aa772083790d72989c52cbd48203483
Le Duy Thuc <leduythuc.cs@gmail.com>
2023-01-27 11:23:55 +0700

    Update abis

--- SDK/abis.ts
+++ SDK/abis.ts
@@ -1,6 +1,6 @@
 import { abi as IProblemAbi } from '../build/artifacts/src/contracts/Problem.sol/Problem.json';
 import { abi as IGateAbi } from '../build/artifacts/src/contracts/Gate.sol/Gate.json';
-import { abi as IUserGateFactoryAbi } from '../build/artifacts/src/interfaces/IUserGateFactory.sol/IUserGateFactory.json';
-import { abi as IProblemFactoryAbi } from '../build/artifacts/src/interfaces/IProblemFactory.sol/IProblemFactory.json';
+import { abi as IUserGateFactoryAbi } from '../build/artifacts/src/contracts/UserGateFactory.sol/UserGateFactory.json';
+import { abi as IProblemFactoryAbi } from '../build/artifacts/src/contracts/ProblemFactory.sol/ProblemFactory.json';
 
 export { IProblemAbi, IGateAbi, IUserGateFactoryAbi, IProblemFactoryAbi };
