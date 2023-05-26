b0dbb9a7174bcc2fe2aaaa2fb7c94370c32f300d
Le Duy Thuc <leduythuc.cs@gmail.com>
2023-05-27 02:09:10 +0700

    Add scripts to update problem deadline

--- scripts/updateProblemDeadline.ts
+++ scripts/updateProblemDeadline.ts
@@ -3,13 +3,17 @@ import hre from 'hardhat';
 import { exit } from 'process';
 import { Gate, Problem, UserGateFactory } from '../typechain-types';
 import { prepEnv } from './env';
-import { deploy } from './helper';
+import { deploy, getContractAt } from './helper';
 import { zeroAddress } from '@nomicfoundation/ethereumjs-util';
 
 async function main() {
     const [deployer]: SignerWithAddress[] = await hre.ethers.getSigners();
     const env = await prepEnv(deployer);
-    await env.problemFactory.createProblem(zeroAddress(), env.nonceManager.nonce());
+    const problemAddress = '0x897e22ffd7761c2acfd7b4c4b459bcd4099b6d2a';
+    const deadline = new Date('2023-10-10');
+
+    const problem = await getContractAt<Problem>('Problem', problemAddress);
+    await problem.setDeadline(Math.trunc(deadline.getTime() / 1000), env.nonceManager.nonce());
 }
 
 main().then(() => {
